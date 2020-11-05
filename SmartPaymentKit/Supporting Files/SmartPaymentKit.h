0b80a9a91d651859d7b0110bc98c375e78f2eed0
Ondřej Beneš <ondrej.benes@airbank.cz>
2020-11-05 12:57:41 +0100

    Transform library to SmartPaymentKit framework to support Carthage
    - update README with Carthage support
    - bump version to 3.0 and remove CocoaPods support

---
+++ SmartPaymentKit/Supporting Files/SmartPaymentKit.h
@@ -0,0 +1,28 @@
+//
+//  SmartPaymentKit.h
+//  SmartPaymentKit
+//
+//  Created by Ondřej Beneš on 05/11/2020.
+//  Copyright © 2020 qr-platba.cz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmartPaymentKit.
+FOUNDATION_EXPORT double SmartPaymentKitVersionNumber;
+
+//! Project version string for SmartPaymentKit.
+FOUNDATION_EXPORT const unsigned char SmartPaymentKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartPaymentKit/PublicHeader.h>
+#import <SmartPaymentKit/SmartPaymentValidator.h>
+#import <SmartPaymentKit/IBANValidator.h>
+#import <SmartPaymentKit/SmartPaymentReader.h>
+#import <SmartPaymentKit/SmartPayment.h>
+#import <SmartPaymentKit/SmartPaymentWriter.h>
+#import <SmartPaymentKit/SmartPaymentUtils.h>
+#import <SmartPaymentKit/SmartPaymentConfiguration.h>
+#import <SmartPaymentKit/SmartPaymentAccount.h>
+#import <SmartPaymentKit/SmartPaymentCZ.h>
+#import <SmartPaymentKit/SmartPaymentConstants.h>
+
