587927bcb2f1371285f7e5491ccbef4b34bbea82
Ondra Beneš <beny@users.noreply.github.com>
2020-12-15 14:19:41 +0100

    Update SmartPaymentReader.m

--- SmartPaymentKit/SmartPaymentReader.m
+++ SmartPaymentKit/SmartPaymentReader.m
@@ -153,7 +153,7 @@
 		}
 		// Quick validate value
 		value = [value stringByReplacingOccurrencesOfString:@"+" withString:@" "];
-        value = [value stringByRemovingPercentEncoding];
+        	value = [value stringByRemovingPercentEncoding];
 		if (value.length == 0) {
 			continue;
 		}
